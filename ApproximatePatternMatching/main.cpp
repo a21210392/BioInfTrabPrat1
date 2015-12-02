2aadeb3b95e69c1fe2338b66510687691cc8f7bb
a21210392 <a21210392@gmail.com>
2015-12-02 15:15:05 +0000

    2015/12/02Project start

---
+++ ApproximatePatternMatching/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: L
+ *
+ * Created on 2 de Dezembro de 2015, 15:13
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
