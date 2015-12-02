00b6d59e6211eaeec977d1216979429ccf42ec4f
a21210392 <a21210392@gmail.com>
2015-12-02 14:20:33 +0000

    2015/12/02Project start

---
+++ ApproximatePatternMatching/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: L
+ *
+ * Created on 2 de Dezembro de 2015, 14:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
