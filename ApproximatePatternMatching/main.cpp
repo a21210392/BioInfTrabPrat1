82e8805ebc7937a0040825dccfc0800f14a4520e
a21210392 <a21210392@gmail.com>
2015-12-02 16:54:38 +0000

    2015/12/02 Project start

--- ApproximatePatternMatching/main.cpp
+++ ApproximatePatternMatching/main.cpp
@@ -1,19 +1,43 @@
-/* 
- * File:   main.cpp
- * Author: L
- *
- * Created on 2 de Dezembro de 2015, 15:13
- */
-
 #include <cstdlib>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
+int patternCounter(string pattern, string sequence, int maxMutations) {
+    int occurrences = 0;
+    int errors;
+    int i, j;
+
+    for (int i = 1; i < sequence.size(); i++) {
+        errors = 0;
+        j = 0;
+        while (j != pattern.size() || errors <= maxMutations) {
+            if (pattern.at(j) != sequence.at(i+j)) {
+                errors += 1;
+            }
+            j++;
+        }
+
+        if (errors <= maxMutations) {
+            occurrences += 1;
+        }
+    }
+    return occurrences;
+}
+
 int main(int argc, char** argv) {
+
+    string pattern, sequence;
+    int occurrences, maxMutations;
+
+    cout << "Pattern, sequence, maximum mutations\n";
+    cin >> pattern >> sequence >> maxMutations;
+
+    //Counts number of occurrences parts of "sequence" match "pattern", with "maxMutations" number of differences
+    occurrences = patternCounter(pattern, sequence, maxMutations);
+
+    cout << "Matches: " + occurrences;
 
     return 0;
 }
-
